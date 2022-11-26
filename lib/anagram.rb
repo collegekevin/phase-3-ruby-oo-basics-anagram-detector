aec60bd611306b1f47dc668208885941b8786ecc
Kevin Price <collegekevin@gmail.com>
2022-11-26 10:31:00 -0800

    commit tree 93864a9b378eda22f628b6e67d1f48d7a476adab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+
+    attr_accessor :word
+    attr_reader 
+
+    def initialize(word)
+        @word = word
+    end
+
+    #binding.pry
+
+    def do_they_match(anagrams)
+        match_word = word.chars
+        anagrams.filter do |word_to_check|
+            match_word.sort == word_to_check.chars.sort
+        end
+    end
+
+end
